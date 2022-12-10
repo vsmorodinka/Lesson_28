d6296538acef60d3b3fb5980590eef461e1c6ab1
vsmorodinka <119528019+vsmorodinka@users.noreply.github.com>
2022-12-10 12:53:51 +0300

    from lesson_28

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Привет, Виктория!") ;
+    /*int my_number=5;
+    double second_number;
+         second_number=22.5;
+        System.out.println("My number is"+ my_number);
+
+        System.out.println("My number is"+ second_number);
+        String my_string = "My first string";
+        System.out.println(my_string);
+        System.out.println(my_string.toLowerCase());
+        System.out.println(my_string.toUpperCase());
+        System.out.println(my_string.length());
+        System.out.println(my_string.charAt(3));*/
+
+        int a=10;
+        int b=3;
+       /* System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);*/
+
+        /*System.out.println(++a);
+        System.out.println(--b);
+
+        System.out.println(a++);
+        System.out.println(b--);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("my text" + a + "my second text");*/
+
+        /*System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);*/
+
+
+    int weekDay=1;
+        if(weekDay==1) {System.out.println("Monday!");}
+        switch (weekDay) {case 1:
+            System.out.println("Monday!");
+            break;}
+
+
     }
-}
